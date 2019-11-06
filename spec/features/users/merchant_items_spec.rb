43cdc9c76fac6d654fa843691898ffbed9ffc8be
Scott Regenthal <scottregenthal@gmail.com>
2019-11-05 22:16:09 -0700

    Change user, order, item_order creation

--- spec/features/users/merchant_items_spec.rb
+++ spec/features/users/merchant_items_spec.rb
@@ -5,33 +5,17 @@ require 'rails_helper'
 RSpec.describe 'As a Merchant Admin' do
   before(:each) do
     @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80_203)
-    @merchant_admin = @meg.users.create(
-      name: 'Admin',
-      address: '123 Main',
-      city: 'Denver',
-      state: 'CO',
-      zip: 80_233,
-      email: 'bobemail.com',
-      password: 'secure',
-      role: 2
-    )
-    @user = User.create!(
-      name: 'User',
-      address: '123 Main',
-      city: 'Denver',
-      state: 'CO',
-      zip: 80_233,
-      email: 'useremail.com',
-      password: 'secure'
-    )
+    @merchant_admin = @meg.users.create(name: 'Admin', email: 'bobemail.com', password: 'secure', role: 2)
+
+    @user_1 = User.create(name: 'User 1', email: 'user_1@user.com', password: 'secure', role: 0)
+    @address_1 = @user_1.addresses.create(address: '123 Main', city: 'Denver', state: 'CO', zip: 80_233)
 
     @tire = @meg.items.create(name: 'Gatorskins', description: "They'll never pop!", price: 100, image: 'https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588', inventory: 12)
     @pump = @meg.items.create(name: 'Bike Pump', description: 'It works fast!', price: 25, image: 'https://images-na.ssl-images-amazon.com/images/I/71Wa47HMBmL._SY550_.jpg', active?: false, inventory: 15)
 
-    @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17_033, status: 'Pending')
-    @order_2 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17_033, status: 'Pending')
-
-    @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+    @order_1 = @user_1.orders.create(name: 'User 1', address_id: @address_1.id, status: 'Pending')
+    @order_2 = @user_1.orders.create(name: 'User 1', address_id: @address_1.id, status: 'Pending')
+    @item_order_1 = @order_1.item_orders.create(item: @tire, price: @tire.price, quantity: 2)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
 
